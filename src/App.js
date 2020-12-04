784dc2057757d5c2aa6b7d9c77018c6253a65927
otsang3 <ohhtsang@gmail.com>
2020-12-04 14:56:03 +0000

    set up react-router-dom, initialised home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" render={() => (
+            <Home/>
+          )}/>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
