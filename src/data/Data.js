fdc26f6353788fae1037044f18ba9827cebeb149
otsang3 <ohhtsang@gmail.com>
2020-12-04 15:41:34 +0000

    drafted layout of the data api

---
+++ src/data/Data.js
@@ -0,0 +1,106 @@
+export const categories = [ "Animals", "Geography", "Science"];
+
+export const topics = {
+    animals: {
+        land: {
+            learningSlides: [
+                "slide 1",
+                "slide 2",
+                "slide 3"
+            ],
+            quiz: [{
+                question: "what is the question?",
+                answers: [
+                    "answer 1",
+                    "answer 2",
+                    "answer 3"
+                ],
+                correctAnswer: "answer 1"
+            }]
+        },
+        sea: {
+            learningSlides: [
+                "slide 1",
+                "slide 2",
+                "slide 3"
+            ],
+            quiz: [{
+                question: "what is the question?",
+                answers: [
+                    "answer 1",
+                    "answer 2",
+                    "answer 3"
+                ],
+                correctAnswer: "answer 1"
+            }]
+        }
+    },
+    geography: {
+        countries: {
+            learningSlides: [
+                "slide 1",
+                "slide 2",
+                "slide 3"
+            ],
+            quiz: [{
+                question: "what is the question?",
+                answers: [
+                    "answer 1",
+                    "answer 2",
+                    "answer 3"
+                ],
+                correctAnswer: "answer 1"
+            }]
+        },
+        cities: {
+            learningSlides: [
+                "slide 1",
+                "slide 2",
+                "slide 3"
+            ],
+            quiz: [{
+                question: "what is the question?",
+                answers: [
+                    "answer 1",
+                    "answer 2",
+                    "answer 3"
+                ],
+                correctAnswer: "answer 1"
+            }]
+        }
+    },
+    science: {
+        chemistry: {
+            learningSlides: [
+                "slide 1",
+                "slide 2",
+                "slide 3"
+            ],
+            quiz: [{
+                question: "what is the question?",
+                answers: [
+                    "answer 1",
+                    "answer 2",
+                    "answer 3"
+                ],
+                correctAnswer: "answer 1"
+            }]
+        },
+        physics: {
+            learningSlides: [
+                "slide 1",
+                "slide 2",
+                "slide 3"
+            ],
+            quiz: [{
+                question: "what is the question?",
+                answers: [
+                    "answer 1",
+                    "answer 2",
+                    "answer 3"
+                ],
+                correctAnswer: "answer 1"
+            }]
+        }
+    },
+}
