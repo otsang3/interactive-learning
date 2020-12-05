1fbc9ed150d9d6f6b193caf8e611b6e090ad8464
otsang3 <ohhtsang@gmail.com>
2020-12-05 11:59:43 +0000

    home.js - added method to capitalise first letter, centered heading, added flexbox to display categories

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,25 @@ import Category from './Category';
 
 function Home() {
 
+    const capitaliseFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
     const renderCategories = () => {
         const categoriesArr = [];
         categories.map((category, index) => categoriesArr.push(
-            <Category key={index} category={category}/>
+            <Category key={index} category={capitaliseFirstLetter(category)}/>
         ));
         return categoriesArr;
     }
 
     return(
         <div>
-            {renderCategories()}
+            <h2>Select a Topic</h2>
+            <div className="cat-container">
+                {renderCategories()}
+            </div>
+            
         </div>
     );
 }
