54728008f3e11c8d7f530df812573336b85520d8
otsang3 <ohhtsang@gmail.com>
2020-12-06 18:34:44 +0000

    set up state, slides and quiz objects in InteractiveDisplay.js

--- src/components/InteractiveDisplay.js
+++ src/components/InteractiveDisplay.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import SlideContainer from './SlideContainer';
+import React, { useState } from 'react';
+import { topics } from '../data/Data';
+import SlidesContainer from './SlidesContainer';
 
 function InteractiveDisplay(props) {
 
-    const { match: { params: { category }} } = props.category
-    const { match: { params: { topic }} } = props.category
+    const initialState = {
+        loadQuiz: false,
+        loadSlides: true
+    };
+
+    const [ state, setState ] = useState(initialState);
+
+    const { match: { params: { category }} } = props.category;
+    const { match: { params: { topic }} } = props.category;
+
+    const slides = topics[category][topic].learningSlides;
+    const quiz = topics[category][topic].quiz;
+
+    if (state.loadQuiz) 
+    return(
+        <p>Quiz loaded</p>
+    )
 
     return(
-        <div>
-            
-        </div>
+        <SlidesContainer slides={slides}/>
     )
 }
 
---
+++ src/components/SlidesContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SlidesContainer() {
+
+    return(
+        <div>
+            Slides loaded
+        </div>
+    )
+}
+
+export default SlidesContainer;
