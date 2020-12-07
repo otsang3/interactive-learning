73d611b050d02ea5a39a44ee2dcb5bd498166bb6
otsang3 <ohhtsang@gmail.com>
2020-12-07 14:05:37 +0000

    added buttons with methods to go to next/previous slide

--- src/components/SlidesContainer.js
+++ src/components/SlidesContainer.js
@@ -10,11 +10,29 @@ function SlidesContainer(props) {
 
     const [ state, setState ] = useState(initialState);
 
-    console.log(state.slides[state.slideNum]);
+    const handleNextSlide = () => {
+        setState(prevState => {
+            return {
+                ...prevState,
+                slideNum: prevState.slideNum + 1
+            }
+        })
+    }
+
+    const handlePrevSlide = () => {
+        setState(prevState => {
+            return {
+                ...prevState,
+                slideNum: prevState.slideNum - 1
+            }
+        })
+    }
 
     return(
         <div>
             {state.slides[state.slideNum]}
+            <button onClick={handlePrevSlide}>Previous</button>
+            <button onClick={handleNextSlide}>Next</button>
         </div>
     )
 }
