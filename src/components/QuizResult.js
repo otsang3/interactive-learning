fe69a8a555df37c63cc000da5adb9641d74f1f62
otsang3 <ohhtsang@gmail.com>
2020-12-29 00:30:33 +0000

    added renderMessage method to populate feedback depending on user's results

--- src/components/QuizResult.js
+++ src/components/QuizResult.js
@@ -2,10 +2,30 @@ import React from 'react';
 
 function QuizResult(props) {
 
+    const renderMessage = () => {
+
+        const userScore = (props.state.score/props.quiz.length);
+        let message = '';
+
+        switch(true) {
+            case (userScore < 0.40):
+                message = 'Better luck next time!'
+                break;
+            case (userScore < 0.60):
+                message = 'Well done!'
+                break;
+            default:
+                message = "Excellent work!"
+                break;
+        }
+
+        return message;
+        
+    }
+
     return(
         <div className="result-container">
-            <h4>You answered {props.state.score} out of {props.quiz.length} questions correctly!</h4>
-            
+            <h4>{renderMessage()}</h4>
         </div>
     )
 }
